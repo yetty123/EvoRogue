0e123a108aa4629ea961f9a798572e8b43cc85be
Scannell24 <Scannell24@gmail.com>
2016-04-24 23:09:33 -0400

    separated Node and EnemyData from Enemy

---
+++ EvoRogue/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class EnemyData
+{
+	public int attackPower;
+	public int defense;
+	public int currentHealth;
+	public int maxHealth;
+	public int damageDone;
+	public int combatTurns;
+	public int energy;
+	public int range;
+	public float accuracy;
+	public bool alive;
+
+	public EnemyData ()
+	{
+		this.attackPower = 1;
+		this.defense = 1;
+		this.maxHealth = 1;
+		this.currentHealth = 1;
+		this.damageDone = 0;
+		this.combatTurns = 0;
+		this.energy = 1;
+		this.accuracy = 1;
+		this.alive = true;
+	}
+
+	// ADD CONSTRUCTORS, SETTERS, GETTERS
+	public EnemyData (int att, int def, int hp, int energy, int range, float accuracy)
+	{
+		this.attackPower = att;
+		this.defense = def;
+		this.currentHealth = hp;
+		this.maxHealth = hp;
+		this.damageDone = 0;
+		this.combatTurns = 0;
+		this.energy = energy;
+		this.range = range;
+		this.accuracy = accuracy;
+		this.alive = true;
+	}
+
+	public void SetAttackPower (int val)
+	{
+		attackPower = val;
+	}
+
+	public void SetDefense (int val)
+	{
+		defense = val;
+	}
+
+	public void SetHealth (int val)
+	{
+		currentHealth = val;
+		maxHealth = val;
+	}
+
+	public void SetEnergy (int val)
+	{
+		energy = val;
+	}
+
+	public void SetAccuracy (float val)
+	{
+		accuracy = val;
+	}
+
+	public void SetRange (int val)
+	{
+		range = val;
+	}
+}
---
+++ EvoRogue/Assets/Scripts/Node.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node
+{
+	public int heuristic;
+	public int movementCost;
+	public int totalCost;
+	public Node parent;
+	public Point coordinate;
+
+	public static bool operator != (Node node1, Node node2)
+	{
+		return !(
+			node1.coordinate == node2.coordinate &&
+			node1.movementCost == node2.movementCost &&
+			node1.totalCost == node2.totalCost &&
+			node1.heuristic == node2.heuristic);
+	}
+
+	public static bool operator == (Node node1, Node node2)
+	{
+		return (
+			node1.coordinate == node2.coordinate &&
+			node1.movementCost == node2.movementCost &&
+			node1.totalCost == node2.totalCost &&
+			node1.heuristic == node2.heuristic);
+	}
+
+	public Node ()
+	{
+		heuristic = 0;
+		movementCost = 0;
+		totalCost = 0;
+		parent = null;
+	}
+
+	public Node (Node otherNode)
+	{
+		coordinate = otherNode.coordinate;
+		heuristic = otherNode.heuristic;
+		movementCost = otherNode.movementCost;
+		totalCost = otherNode.heuristic + otherNode.movementCost;
+		parent = otherNode.parent;
+	}
+
+	public Node (Point Coordinate, int Heuristic, int MovementCost)
+	{
+		coordinate = Coordinate;
+		heuristic = Heuristic;
+		movementCost = MovementCost;
+		totalCost = Heuristic + MovementCost;
+		parent = this;
+	}
+
+	public Node (Point Coordinate, int Heuristic, int MovementCost, Node Parent)
+	{
+		coordinate = Coordinate;
+		heuristic = Heuristic;
+		movementCost = MovementCost;
+		totalCost = Heuristic + MovementCost;
+		parent = Parent;
+	}
+}
